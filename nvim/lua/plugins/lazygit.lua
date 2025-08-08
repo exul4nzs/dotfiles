98f04454dadf2f825de0cae5d213ad513f14a37e
Bartley Josh <bartleyjosh@proton.me>
2025-08-08 16:41:17 +0800

    Create lazygit.lua

---
+++ nvim/lua/plugins/lazygit.lua
@@ -0,0 +1,17 @@
+-- LazyGit integration with Telescope
+return {
+		"kdheepak/lazygit.nvim",
+		keys = {
+			{
+				"<leader>lg",
+				"<cmd>LazyGit<cr>",
+        desc = "Open LazyGit",
+				silent = true,
+				noremap = true,
+			},
+		},
+		-- optional for floating window border decoration
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+}
