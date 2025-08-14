3f8860d4ef5cdc9f04ef4b316d43e039703b0b8a
Bartley Josh <bartleyjosh@proton.me>
2025-08-14 18:40:44 +0800

    Update plugins.lua
    wtf was that?

--- nvim/lua/plugins/plugins.lua
+++ nvim/lua/plugins/plugins.lua
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-﻿return {
-=======
 return {
->>>>>>> 27faf42fcc5bc51571f09cc7aea64aa55b18a3e1
   {
     'MeanderingProgrammer/render-markdown.nvim',
     event = "VeryLazy",
@@ -41,7 +37,6 @@ return {
             enabled = true,
             leave_dirs_open = true,
           },
-<<<<<<< HEAD
           window = {
             mappings = {
               ["<cr>"] = "toggle_node",  -- Verify this mapping exists
@@ -52,8 +47,6 @@ return {
           mappings = {
             ["<cr>"] = "toggle_node",  -- Verify this mapping exists
           }
-=======
->>>>>>> 27faf42fcc5bc51571f09cc7aea64aa55b18a3e1
         },
       })
     end,
@@ -107,14 +100,11 @@ return {
     "folke/noice.nvim",
     event = "VeryLazy",
     opts = {
-<<<<<<< HEAD
       lsp = {
         signature = { enabled = false },
         hover = { enabled = false },
         progress = { enabled = false },
       },
-=======
->>>>>>> 27faf42fcc5bc51571f09cc7aea64aa55b18a3e1
       routes = {
         {
           filter = { event = "notify", find = "No information available" },
@@ -183,7 +173,6 @@ return {
           lua = { "stylua" },
           svelte = { { "prettierd", "prettier", stop_after_first = true } },
           astro = { { "prettierd", "prettier", stop_after_first = true } },
-<<<<<<< HEAD
           cs = { "csharpier" },
           -- javascript = { { "prettierd", "prettier", stop_after_first = true } },
           -- typescript = { { "prettierd", "prettier", stop_after_first = true } },
@@ -195,13 +184,11 @@ return {
           javascriptreact = { "biome" },
           typescriptreact = { "biome" },
           json = { "biome" },
-=======
           javascript = { { "prettierd", "prettier", stop_after_first = true } },
           typescript = { { "prettierd", "prettier", stop_after_first = true } },
           javascriptreact = { { "prettierd", "prettier", stop_after_first = true } },
           typescriptreact = { { "prettierd", "prettier", stop_after_first = true } },
           json = { { "prettierd", "prettier", stop_after_first = true } },
->>>>>>> 27faf42fcc5bc51571f09cc7aea64aa55b18a3e1
           graphql = { { "prettierd", "prettier", stop_after_first = true } },
           java = { "google-java-format" },
           kotlin = { "ktlint" },
@@ -220,15 +207,12 @@ return {
           go = { "gofmt" },
           xml = { "xmllint" },
         },
-<<<<<<< HEAD
         formatters = {
           csharpier = {
             command = "dotnet-csharpier",
             args = {  "--write-stdout" },
           }
         }
-=======
->>>>>>> 27faf42fcc5bc51571f09cc7aea64aa55b18a3e1
       })
 
       vim.keymap.set({ "n", "v" }, "<leader>l", function()
@@ -471,7 +455,6 @@ return {
       "weilbith/neotest-gradle",
       "nvim-neotest/neotest-go",
     },
-<<<<<<< HEAD
     opts = {
       adapters = {
         ["rustaceanvim.neotest"] = {},
@@ -497,9 +480,7 @@ return {
         },
       },
     },
-=======
     opts = {},
->>>>>>> 27faf42fcc5bc51571f09cc7aea64aa55b18a3e1
     config = function()
       local neotest = require("neotest")
 
@@ -716,28 +697,22 @@ return {
 
       mason_tool_installer.setup({
         ensure_installed = {
-<<<<<<< HEAD
           "phpcs",
           "php-cs-fixer",
           "standardrb",
           "prettier",
           "goimports",
           "gofumpt",
-=======
           "standardrb",
           "prettier",
->>>>>>> 27faf42fcc5bc51571f09cc7aea64aa55b18a3e1
           "prettierd",
           "ktlint",
           "eslint_d",
           "google-java-format",
           "htmlbeautifier",
           "beautysh",
-<<<<<<< HEAD
           "java-debug-adapter",
           "java-test",
-=======
->>>>>>> 27faf42fcc5bc51571f09cc7aea64aa55b18a3e1
           "css-lsp",
           "tailwindcss-language-server",
           "typescript-language-server",
@@ -821,10 +796,7 @@ return {
     lazy = false
   },
   {'bfrg/vim-cpp-modern'},
-<<<<<<< HEAD
-=======
   {'p00f/clangd_extensions.nvim'},
->>>>>>> 27faf42fcc5bc51571f09cc7aea64aa55b18a3e1
   {
     "kristijanhusak/vim-dadbod-ui",
     dependencies = {
@@ -842,7 +814,6 @@ return {
       { "<leader>Dbi", "<cmd>DBUILastQueryInfo<cr>", desc = "DB UI Last Query Info" },
     },
   },
-<<<<<<< HEAD
   {
     "nvzone/showkeys",
     event = "VeryLazy",
@@ -931,7 +902,7 @@ return {
   {
     "p00f/clangd_extensions.nvim",
     lazy = true,
-    config = function() 
+    config = function()
       require("clangd_extensions").setup({})
     end,
 
@@ -1461,6 +1432,4 @@ return {
   --     },
   --   },
   -- },
-=======
->>>>>>> 27faf42fcc5bc51571f09cc7aea64aa55b18a3e1
 }
