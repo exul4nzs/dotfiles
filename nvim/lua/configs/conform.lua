a901e5ab3bfd62cf563eaa89c57320e7c0699278
Bartley Josh <bartleyjosh@proton.me>
2025-08-15 20:18:30 +0800

    Update conform.lua
    removed anoyying <<<<<<<<<<<<<<<<<

--- nvim/lua/configs/conform.lua
+++ nvim/lua/configs/conform.lua
@@ -1,63 +1,37 @@
 local options = {
   formatters_by_ft = {
     lua = { "stylua" },
-<<<<<<< HEAD
-    svelte = { { "prettierd", "prettier", stop_after_first = true } },
-    astro = { { "prettierd", "prettier", stop_after_first = true } },
+    svelte = { "prettier" },
+    astro = { "prettier" },
     cs = { "csharpier" },
-    -- javascript = { { "prettierd", "prettier", stop_after_first = true } },
-    -- typescript = { { "prettierd", "prettier", stop_after_first = true } },
-    -- javascriptreact = { { "prettierd", "prettier", stop_after_first = true } },
-    -- typescriptreact = { { "prettierd", "prettier", stop_after_first = true } },
-    -- json = { { "prettierd", "prettier", stop_after_first = true } },
     javascript = { "biome" },
     typescript = { "biome" },
     javascriptreact = { "biome" },
     typescriptreact = { "biome" },
     json = { "biome" },
-    graphql = { { "prettierd", "prettier", stop_after_first = true } },
+    graphql = { "prettier" },
     java = { "google-java-format" },
     kotlin = { "ktlint" },
     ruby = { "standardrb" },
-    markdown = { { "prettierd", "prettier", stop_after_first = true } },
+    markdown = { "prettier" },
     erb = { "htmlbeautifier" },
     html = { "htmlbeautifier" },
     bash = { "beautysh" },
-    zsh = { "beautysh" },
     proto = { "buf" },
     rust = { "rustfmt" },
     yaml = { "yamlfix" },
     toml = { "taplo" },
-    css = { { "prettierd", "prettier", stop_after_first = true } },
-    scss = { { "prettierd", "prettier", stop_after_first = true } },
+    css = { "prettier" },
+    scss = { "prettier" },
     sh = { "shellcheck" },
     go = { "gofmt" },
-    sh = { "shfmt" },
     xml = { "xmllint" },
-  },
-  formatters = {
-    csharpier = {
-      command = "dotnet-csharpier",
-      args = {  "--write-stdout" },
-    }
   },
 
   format_on_save = {
-    -- These options will be passed to conform.format()
     timeout_ms = 500,
-    lsp_fallback = true,
+    lsp_format = "fallback",
   },
-=======
-    -- css = { "prettier" },
-    -- html = { "prettier" },
-  },
-
-  -- format_on_save = {
-  --   -- These options will be passed to conform.format()
-  --   timeout_ms = 500,
-  --   lsp_fallback = true,
-  -- },
->>>>>>> 27faf42fcc5bc51571f09cc7aea64aa55b18a3e1
 }
 
 return options
