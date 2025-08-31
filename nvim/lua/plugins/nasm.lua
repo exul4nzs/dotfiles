14a3b5e54d15a0a9662f2e9ee67ad782bda159db
exul4nzs <bartleyjosh@proton.me>
2025-08-31 16:46:26 +0800

    Added markview.lua and nasm.lua

---
+++ nvim/lua/plugins/nasm.lua
@@ -0,0 +1,9 @@
+return {
+  "Who5673/who5673-nasm", -- Or git@github.com:Who5673/who5673-nasm if you use SSH Key (recommended)
+  dependencies = {
+    "L3MON4D3/LuaSnip",
+    "saghen/blink.nvim",
+  },
+  ft = "nasm",
+  lazy = true,
+}
