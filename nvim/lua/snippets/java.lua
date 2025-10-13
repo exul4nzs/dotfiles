90cc474757571bba4f0dc2bd384ea54264b27ac2
exul4nzs <bartleyjosh@proton.me>
2025-10-13 14:10:23 +0800

    Added necessary print & printf for blink.cmp

--- nvim/lua/snippets/java.lua
+++ nvim/lua/snippets/java.lua
@@ -7,8 +7,7 @@ local fmt = require("luasnip.extras.fmt").fmt
 return {
   -- Snippet for a basic Javadoc comment
   s(
-    "javadoc",
-    fmt(
+    "javadoc", fmt(
       [[
     /**
      * {}
@@ -45,6 +44,53 @@ return {
       }
     )
   ),
+
+
+  -- Snippet for multiple else-if statements
+  s(
+    "ifelseif",
+    fmt(
+      [[
+if ({}) {{
+  {}
+}} else if ({}) {{
+  {}
+}} else if ({}) {{
+  {}
+}} else {{
+  {}
+}}
+{}
+]],
+      {
+        i(1, "condition"),
+        i(2, "// code"),
+        i(3, "condition"),
+        i(4, "// code"),
+        i(5, "condition"),
+        i(6, "// code"),
+        i(7, "// code"),
+        i(0),
+      }
+    )
+  ),
+  -- Snippet for System.out.print
+  s(
+    "print",
+    fmt([[System.out.print({});]], {
+      i(1, '"text"'),
+    })
+  ),
+
+  -- Snippet for System.out.printf
+  s(
+    "printf",
+    fmt([[System.out.printf({}, {});]], {
+      i(1, '"format"'),
+      i(2, "args"),
+    })
+  ),
+
 }, {
   -- This is the key part: these snippets will only be available in Java files
   type = "autosnippets",
